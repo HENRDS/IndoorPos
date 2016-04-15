26eca9c0ffc3654e193337c56218b43c257d6992
Henry R. da Silva <henryqx@hotmail.com>
2016-04-15 17:59:14 -0300

    Grayscale test

--- main.cpp
+++ main.cpp
@@ -13,21 +13,41 @@
 
 
 #include "opencv2/opencv.hpp"
+#include <iostream>
 
 using namespace cv;
 
+//inline uchar avg (uchar * vec) {
+//    return (*vec + *(vec+1) + *(vec+2)) / 3;
+//}
+//
+//Mat * grayscale (Mat &frame) {
+//    Mat * result = new Mat(frame.size(), CV_8UC1);
+//    std::cout << frame.rows << "x" << frame.cols << std::endl;
+//    for (int i=0; i < frame.rows; i++) {
+//        uchar * res= result->ptr(i);
+//        uchar * frptr = frame.ptr(i);
+//        for (int j = 0; j < frame.cols; j++) {  
+//            if ((i  >  1920) || (j > 1080))
+//                std::cout << "EOI" << std::endl;
+//            *res = avg(frptr+(3*i));
+//            std::cout << *res << std::endl;
+//        }
+//    }
+//    return result;
+//}
 inline uchar avg (Vec3b & vec) {
     return (vec(0) + vec(1) + vec(2)) / 3;
 }
 
 Mat * grayscale (Mat &frame) {
-    int tp = frame.type();
     Mat * result = new Mat(frame.size(), CV_8UC1);
     for (int i=0; i < frame.size().height; i++) {
         for (int j = 0; j < frame.size().width; j++) {
             result->at<uchar>(i, j) = avg(frame.at<Vec3b>(i, j));
         }
     }
+    return result;
 }
 // Blur
 void gaussian_blur(Mat * frame) {
@@ -50,7 +70,15 @@ int main(int argc, char** argv) {
         return -1;
     
     Mat frame;
-    cap >> frame;
+    for (int i = 0; i < 10; i++) {
+        cap >> frame;
+    }
+
+    
+    imwrite( "Image.jpg", frame);
+    Mat * im = grayscale(frame);
+    
+    imwrite( "Gray_Image.jpg", *im);
     
     return 0;
 }
--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -35,7 +35,6 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/ImgProcessor.o \
 	${OBJECTDIR}/main.o
 
 
@@ -62,11 +61,6 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/indoorpos: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/indoorpos ${OBJECTFILES} ${LDLIBSOPTIONS}
-
-${OBJECTDIR}/ImgProcessor.o: ImgProcessor.cpp 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/ImgProcessor.o ImgProcessor.cpp
 
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
